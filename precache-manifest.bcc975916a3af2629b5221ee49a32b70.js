ba8d019d841c612b4bd56c8c825108e15ce0bf23
Tejaswi2737 <41263776+Tejaswi2737@users.noreply.github.com>
2020-06-15 12:51:33 +1000

    Updates

---
+++ precache-manifest.bcc975916a3af2629b5221ee49a32b70.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6eac0b386b2a00f88603d297f32836",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ee0dff62127db3bc5666",
+    "url": "/Portfolio/static/css/2.92a0e85f.chunk.css"
+  },
+  {
+    "revision": "04a0e8d02e31d761bc6e",
+    "url": "/Portfolio/static/css/main.c7a31749.chunk.css"
+  },
+  {
+    "revision": "ee0dff62127db3bc5666",
+    "url": "/Portfolio/static/js/2.78a76288.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.78a76288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a0e8d02e31d761bc6e",
+    "url": "/Portfolio/static/js/main.5f8652e5.chunk.js"
+  },
+  {
+    "revision": "8336f3dd5187fb202881",
+    "url": "/Portfolio/static/js/runtime-main.0b8f270d.js"
+  },
+  {
+    "revision": "79c5664c24c6ea121663f3262a741fe0",
+    "url": "/Portfolio/static/media/Resume.79c5664c.pdf"
+  },
+  {
+    "revision": "356b6fb24f4724bed76073c9680a92d0",
+    "url": "/Portfolio/static/media/code-icon.356b6fb2.svg"
+  },
+  {
+    "revision": "e0995c3d451dd2572964d055f34332cc",
+    "url": "/Portfolio/static/media/covid_web.e0995c3d.PNG"
+  }
+]);
